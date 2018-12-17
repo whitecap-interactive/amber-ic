49b6310eeb7315dcc1c91a8d33179b34d623f173
dvdwhite <david@dvdwhite.com>
2018-12-17 07:52:41 -0700

    Questions sidebar

--- page.php
+++ page.php
@@ -32,11 +32,12 @@ get_header(); ?>
 
 			<?php endwhile; // end of the loop. ?>
 
-			<?php get_sidebar(); ?>	
+				
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	
 </div><!-- #page container -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
