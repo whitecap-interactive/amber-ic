e56b91338b8793a1905494f14b4aa9f8bbbaa62c
Dave White <david@dvdwhite.com>
2019-01-01 12:23:28 -0700

    removed sidebar for tribal connections

---
+++ category-tribal-connections.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package amber-ic
+ */
+
+get_header(); ?>
+
+<div class="archive-container">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+					<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						/*
+						 * Include the Post-Type-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'archive' );
+
+					endwhile;
+
+					the_posts_navigation();
+
+					?>
+
+					
+					
+			<?php					
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div><!-- #archive container -->
+
+<?php
+get_footer();
