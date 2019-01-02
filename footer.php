ad16271e437bf9a4b25ffe62cd2fb0bfc96001b3
Dave White <david@dvdwhite.com>
2019-01-02 07:31:23 -0700

    last minute styles

--- footer.php
+++ footer.php
@@ -14,16 +14,27 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="page-container flex-container">
-			<div class="logo">
-				<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-color.png" width="150" border="0" alt="AMBER Alert in Indian Country" /></a>
+		<div class="page-container desktop-footer">
+			<div class="flex-container">
+				<div class="logo">
+					<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-color.png" width="150" border="0" alt="AMBER Alert in Indian Country" /></a>
+				</div>
+				<div class="logo">
+					<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-ojpcolor.png" width="100" border="0" alt="OJP" /></a>
+				</div>
+				<div class="logo" style="padding-top:20px">
+					<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-ojjdp.png" width="120" border="0" alt="OJJDP" /></a>
+				</div>
+				<div class="address">
+					AMBER Alert Training & Technical Assistance Program<br />
+					P.O. Box 2277<br />
+					Appleton, WI 54912-2277<br />
+					877-712-6237 (877-71-AMBER)<br />
+					<a href="mailto:askamber@fvtc.edu?subject=AMBER-IC Website Inquiry">askamber@fvtc.edu</a>
+				</div>
 			</div>
-			<div class="logo">
-				<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-ojpcolor.png" width="100" border="0" alt="OJP" /></a>
-			</div>
-			<div class="logo" style="padding-top:20px">
-				<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-ojjdp.png" width="120" border="0" alt="OJJDP" /></a>
-			</div>
+		</div>
+		<div class="page-container mobile-footer">
 			<div class="address">
 				AMBER Alert Training & Technical Assistance Program<br />
 				P.O. Box 2277<br />
@@ -31,7 +42,20 @@
 				877-712-6237 (877-71-AMBER)<br />
 				<a href="mailto:askamber@fvtc.edu?subject=AMBER-IC Website Inquiry">askamber@fvtc.edu</a>
 			</div>
+			<hr />
+			<div class="flex-container">
+				<div class="logo">
+					<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-color.png" width="150" border="0" alt="AMBER Alert in Indian Country" /></a>
+				</div>
+				<div class="logo">
+					<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-ojpcolor.png" width="100" border="0" alt="OJP" /></a>
+				</div>
+				<div class="logo" style="padding-top:20px">
+					<a href="/"><img src="/wp-content/themes/amber-ic/images/logo-ojjdp.png" width="120" border="0" alt="OJJDP" /></a>
+				</div>
+			</div>
 		</div>
+	
 		<div class="site-info">
 			<div class="footer-copyright">
 				Copyright &copy; <?php echo date("Y");?> AMBER Alert Training and Technical Assistance Program
